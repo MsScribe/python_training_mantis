06ade71c0a9d9dc27ad879cf0333e8ddcaf4ae59
Scherbakova Natalya <19natasha94@bk.ru>
2020-08-05 15:38:31 +0300

    Добавлены проверки через Soap

--- conftest.py
+++ conftest.py
@@ -4,6 +4,7 @@ import os.path
 import importlib
 from fixture.application import Application
 import jsonpickle
+import ftputil
 
 fixture = None
 target = None
@@ -18,17 +19,45 @@ def load_config(file):
     return target
 
 
+@pytest.fixture(scope="session")
+def config(request):
+    return load_config(request.config.getoption("--target"))
+
+
 # (scope="session") - запуск браузера на сессию, а не на каждый тест
 @pytest.fixture
-def app(request):
+def app(request, config):
     global fixture
     browser = request.config.getoption("--browser")
-    web_config = load_config(request.config.getoption("--target"))["web"]
-    web_config_admin = load_config(request.config.getoption("--target"))["webadmin"]
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, base_url=web_config["baseURL"])
-    fixture.session.ensure_login(username=web_config_admin["username"], password=web_config_admin["password"])
+        fixture = Application(browser=browser, config=config)
+    fixture.session.ensure_login(username=config["webadmin"]["username"], password=config["webadmin"]["password"])
     return fixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def configure_server(request, config):
+    install_server_configuration(config["ftp"]["host"], config["ftp"]["username"], config["ftp"]["password"])
+    def fin():
+        restore_server_configuration(config["ftp"]["host"], config["ftp"]["username"], config["ftp"]["password"])
+    request.addfinalizer(fin)
+
+
+def install_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.bak"):
+            remote.remove("config_inc.php.bak")
+        if remote.path.isfile("config_inc.php"):
+            remote.rename("config_inc.php", "config_inc.php.bak")
+        remote.upload(os.path.join(os.path.dirname(__file__), "resources/config_inc.php"), "config_inc.php")
+
+
+def restore_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.bak"):
+            if remote.path.isfile("config_inc.php"):
+                remote.remove("config_inc.php")
+            remote.rename("config_inc.php.bak", "config_inc.php")
 
 
 @pytest.fixture(scope="session", autouse=True)
--- test/test_add_project.py
+++ test/test_add_project.py
@@ -4,13 +4,15 @@ from model.mantis import MantisProject
 def test_add_project(app):
     mantis = MantisProject(projectname=app.mantis.random_string("name_", 10), description=app.mantis.random_string("description_", 20))
     app.mantis.open_project_page()
-    old_mantis = app.mantis.get_project_list()
+    old_mantis = app.soap.get_project_list()
+    print(old_mantis)
     if app.mantis.get_existing_project(mantis.projectname):
         app.mantis.delete_project_mantis(mantis.projectname)
-        old_mantis = app.mantis.get_project_list()
+        old_mantis = app.soap.get_project_list()
     app.mantis.open_create_project_page()
     app.mantis.create_project_mantis(mantis)
     old_mantis.append(mantis)
     app.mantis.open_project_page()
-    new_mantis = app.mantis.get_project_list()
+    new_mantis = app.soap.get_project_list()
+    print(new_mantis)
     assert sorted(old_mantis, key=MantisProject.id_or_max) == sorted(new_mantis, key=MantisProject.id_or_max)
