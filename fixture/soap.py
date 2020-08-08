48353a7651afdb930e23f53037636886993d30b9
Scherbakova Natalya <19natasha94@bk.ru>
2020-08-08 17:55:50 +0300

    Поправлен путь при получении списка

--- fixture/soap.py
+++ fixture/soap.py
@@ -8,7 +8,7 @@ class SoapHelper:
         self.app = app
 
     def can_login(self, username, password):
-        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.app.base_url + "/api/soap/mantisconnect.php?wsdl")
         try:
             client.service.mc_login(username, password)
             return True
@@ -16,7 +16,7 @@ class SoapHelper:
             return False
 
     def get_project_list(self):
-        client = Client('http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl')
+        client = Client(self.app.base_url + "/api/soap/mantisconnect.php?wsdl")
         project_list = []
         for project in client.service.mc_projects_get_user_accessible(self.app.config['webadmin']['username'], self.app.config['webadmin']['password']):
             project_list.append(MantisProject(projectname=project.name, description=project.description, id=project.id))
